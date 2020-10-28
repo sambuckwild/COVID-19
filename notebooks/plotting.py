d201e2286fb1cb7b9e7c44875f0b4997c605ba45
Sam Buck <sambuckwild@gmail.com>
2020-10-28 10:25:11 -0600

    update readme

--- notebooks/plotting.py
+++ notebooks/plotting.py
@@ -48,65 +48,65 @@ if __name__ == '__main__':
     us_plot = '../images/us_daily_cases.png'
     a = us_covid_total[us_case_date_col]
     b = us_covid_total['new_case']
-    fig, ax = plt.subplots(figsize=(12,8))
+    fig, ax = plt.subplots(figsize=(6,4))
     daily_case_bar(us_covid_total, a, b, '#FBC00C', 'United States COVID-19 Daily Cases', 'United States', ax)
     image_of_plot(us_plot)
     #canada daily cases plot
     canada_plot = '../images/canada_daily_cases.png'
     c = canada_covid_total[can_date_col]
     d = canada_covid_total['numtoday']
-    fig, ax = plt.subplots(figsize=(12,8))
+    fig, ax = plt.subplots(figsize=(6,4))
     daily_case_bar(canada_covid_total, c, d, '#A62205', 'Canada COVID-19 Daily Cases', 'Canada', ax)
     image_of_plot(canada_plot)
     #australia daily cases plot
     aus_plot = '../images/aus_daily_cases.png'
     e = aus_covid[aus_date_col]
     f = aus_covid['confirmed']
-    fig, ax = plt.subplots(figsize=(12,8))
+    fig, ax = plt.subplots(figsize=(6,4))
     daily_case_bar(aus_covid, e, f, '#1A89F4', 'Australia COVID-19 Daily Cases', 'Australia', ax)
     image_of_plot(aus_plot)
     #new zealand daily cases plot
     nz_plot = '../images/nz_daily_cases.png'
     x = nz_covid_total[nz_date_col]
     y = nz_covid_total['Daily_conf']
-    fig, ax = plt.subplots(figsize=(12,8))
+    fig, ax = plt.subplots(figsize=(6,4))
     daily_case_bar(nz_covid_total, x, y, 'black', 'New Zealand COVID-19 Daily Cases', 'New Zealand', ax)
     image_of_plot(nz_plot)
     #colorado daily cases plot
     colorado_plot = '../images/colorado_daily_cases.png'
     g = co_covid[us_case_date_col]
     h = co_covid['new_case']
-    fig, ax = plt.subplots(figsize=(12,8))
+    fig, ax = plt.subplots(figsize=(6,4))
     daily_case_bar(co_covid, g, h, 'm', 'Colorado COVID-19 Daily Cases', 'Colorado', ax)
     image_of_plot(colorado_plot)
 
     #plot with four countries
     merge_plot_1 = '../images/four_merge_daily_cases.png'
     fig, ax = plt.subplots(figsize=(12,8))
-    daily_case_bar(us_covid_total, a, b, '#FBC00C', 'COVID-19 Daily Cases', 'United States', ax)
-    daily_case_bar(canada_covid_total, c, d, '#A62205', 'COVID-19 Daily Cases', 'Canada', ax)
-    daily_case_bar(aus_covid, e, f, '#1A89F4', 'COVID-19 Daily Cases', 'Australia', ax)
-    daily_case_bar(nz_covid_total, x, y, 'black', 'COVID-19 Daily Cases', 'New Zealand', ax)
+    daily_case_bar(us_covid_total, a, b, '#FBC00C', 'Comparison of COVID-19 Daily Cases: Four Countries', 'United States', ax)
+    daily_case_bar(canada_covid_total, c, d, '#A62205', 'Comparison of COVID-19 Daily Cases: Four Countries', 'Canada', ax)
+    daily_case_bar(aus_covid, e, f, '#1A89F4', 'Comparison of COVID-19 Daily Cases: Four Countries', 'Australia', ax)
+    daily_case_bar(nz_covid_total, x, y, 'black', 'Comparison of COVID-19 Daily Cases: Four Countries', 'New Zealand', ax)
     image_of_plot(merge_plot_1)
 
     #plot with canada, aus, nz
     merge_plot_2 = '../images/three_merge_daily_cases.png'
     fig, ax = plt.subplots(figsize=(12,8))
-    daily_case_bar(canada_covid_total, c, d, '#A62205', 'COVID-19 Daily Cases', 'Canada', ax)
-    daily_case_bar(aus_covid, e, f, '#1A89F4', 'COVID-19 Daily Cases', 'Australia', ax)
-    daily_case_bar(nz_covid_total, x, y, 'black', 'COVID-19 Daily Cases', 'New Zealand', ax)
+    daily_case_bar(canada_covid_total, c, d, '#A62205', 'Comparison of COVID-19 Daily Cases: Three Countries', 'Canada', ax)
+    daily_case_bar(aus_covid, e, f, '#1A89F4', 'Comparison of COVID-19 Daily Cases: Three Countries', 'Australia', ax)
+    daily_case_bar(nz_covid_total, x, y, 'black', 'Comparison of COVID-19 Daily Cases: Three Countries', 'New Zealand', ax)
     image_of_plot(merge_plot_2)
 
     #plot with aus and nz
     merge_plot_3 = '../images/two_merge_daily_cases.png'
     fig, ax = plt.subplots(figsize=(12,8))
-    daily_case_bar(aus_covid, e, f, '#1A89F4', 'COVID-19 Daily Cases', 'Australia', ax)
-    daily_case_bar(nz_covid_total, x, y, 'black', 'COVID-19 Daily Cases', 'New Zealand', ax)
+    daily_case_bar(aus_covid, e, f, '#1A89F4', 'Comparison of COVID-19 Daily Cases: Two Countries', 'Australia', ax)
+    daily_case_bar(nz_covid_total, x, y, 'black', 'Comparison of COVID-19 Daily Cases: Two Countries', 'New Zealand', ax)
     image_of_plot(merge_plot_3)
 
     #plot with new zealand and colorado
     merge_plot_4 = '../images/co_nz_merge_daily_cases.png'
     fig, ax = plt.subplots(figsize=(12,8))
-    daily_case_bar(co_covid, g, h, 'm', 'COVID-19 Daily Cases', 'Colorado', ax)
-    daily_case_bar(nz_covid_total, x, y, 'black', 'COVID-19 Daily Cases Comparison', 'New Zealand', ax)
+    daily_case_bar(co_covid, g, h, 'm', 'Comparison of COVID-19 Daily Cases: New Zealand + Colorado', 'Colorado', ax)
+    daily_case_bar(nz_covid_total, x, y, 'black', 'Comparison of COVID-19 Daily Cases: New Zealand + Colorado', 'New Zealand', ax)
     image_of_plot(merge_plot_4)
